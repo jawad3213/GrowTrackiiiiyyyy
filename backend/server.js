4f9225286054e89f17ea4f8558966c3051b7137e
El Ghrich Salman <elghrichsalman2004@gmail.com>
2025-04-17 21:44:39 +0000

    System d'authentification

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const PORT = process.env.PORT;
+const authController = require('./controllers/authController');
+const verify=require('./middlewares/VerifyToken')
+
+app.use(express.json());
+app.use(cookieParser());
+
+const AuthRoute = require("./routes/AuthRoute");
+app.use("/auth", AuthRoute)
+
+
+app.post('/reset-pass-email',verify.verifyToken, authController.ResetPassEmail);
+
+app.listen(PORT, () => {
+    console.log(`Server Running on http://localhost:${PORT}`);
+});
