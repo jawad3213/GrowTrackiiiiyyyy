f21e3b6a1a4974872e95d9f0a59f910dc8d4808c
SaraLrs <saralaaroussi8@gmail.com>
2025-04-28 13:39:52 +0000

    sara darate mouchekile

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted} from 'vue';
+import { useAuthStore} from './stores/auth';
+const store = useAuthStore();
+onMounted(async ()=> {await store.fetchUser()}) //on fait appelle a fetchUser dés que l'application est montée etg à chaque rechargement 
 </script>
 
 <template>
-  
-  <div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-  
   <div>
-    <div class="text-xl font-medium text-black dark:text-white">ChitChat</div>
-    <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
+    <router-view/>
   </div>
-</div>
-  <RouterView />
+
 </template>
 
-<style scoped>
-
-</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,96 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {  createRouter, createWebHistory } from "vue-router";
+import login from "../components/login.vue";
+import resetPassword from "../components/resetPassword.vue";
+import home from "../components/home.vue";
+import forgotPassword from "../components/forgotPassword.vue";
+import check from "../components/check.vue";
+import contactUs from "../components/contactUs.vue";
+import Header from "../components/Header.vue";
+import AddStudentModal from "@/components/AddStudentModal.vue";
+import AddProfessorModal from "@/components/AddProfessorModal.vue";
+import AddSupervisorModal from "@/components/AddSupervisorModal.vue";
+import AddSkill from "@/components/AddSkill.vue";
+import signal from "@/components/signal.vue";
+import AddCoach from "@/components/AddCoach.vue";
+import AddField from "@/components/AddField.vue";
+
+const routes = [
+    {
+        name: "Home",
+        component: home,
+        path:"/",
+
+    },
+    {
+        name: "login",
+        component: login,
+        path:"/login",
+    },
+    {
+        name: "restepass",
+        component: resetPassword,
+        path:"/resetpass",
+    },
+    {
+        name: "forgotepass",
+        component: forgotPassword,
+        path:"/forgotpass",
+    },
+    {
+        name: "check",
+        component: check,
+        path:"/check",
+    },
+    {
+        name: "contactUs",
+        component: contactUs,
+        path:"/contactUs",
+    },
+    {
+        name: "Header",
+        component: Header,
+        path:"/Header",
+    },
+    {
+        name: "AddStudent",
+        component: AddStudentModal,
+        path:"/AddStudent",
+    },
+    {
+        name: "AddProfessor",
+        component: AddProfessorModal,
+        path:"/AddProfessor",
+    },
+    {
+        name: "AddSupervisor",
+        component: AddSupervisorModal,
+        path:"/AddSupervisor",
+    },
+    {
+        name: "AddSkill",
+        component: AddSkill,
+        path:"/AddSkill",
+    },
+    {
+        name: "AddCoach",
+        component: AddCoach,
+        path:"/AddCoach",
+    },
+    {
+        name: "AddField",
+        component: AddField,
+        path:"/AddField",
+    },
+    {
+        name: "signal",
+        component: signal,
+        path:"/signal",
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      
-    },
-  ],
-})
+    history:createWebHistory(),
+    routes,
+});
 
-export default router
+export default router;
