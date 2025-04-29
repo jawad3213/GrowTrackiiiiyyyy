1f9606fd1032b1532f07d08e9c3efba1e77119a7
El Hail Jaouad <elhailjaouad123@gmail.com>
2025-04-29 12:42:50 +0100

    raja3te lmain lblaasto

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    darkMode: 'class', // <-- OBLIGATOIRE pour que ton bouton toggle fonctionne
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
