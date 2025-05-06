8c03a1a279bf6f8f08deaaf4b082539ab27d0cee
El Hail Jaouad <elhailjaouad123@gmail.com>
2025-05-06 16:00:54 +0100

    some changes

--- backend/routes/HashRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const HashController = require('../controllers/HashController.js');
-router.post('/', HashController.hachage);
-
-
-module.exports = router;
--- backend/routes/contactusRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const contactusController = require('../controllers/contactusController');
-
-router.post('/',contactusController.contactus);
-
-module.exports = router;
